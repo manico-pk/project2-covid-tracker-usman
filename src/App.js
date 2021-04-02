f85211a831d31c387b36c04458025fa34bb857b4
manico-pk <62180781+manico-pk@users.noreply.github.com>
2021-04-02 17:14:50 +0500

    boilerplate
    correction in boiler plate to implement ci/cd

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Muhammad Usman
+          Muhammad Usman Rasheed
         </p>
         <a
           className="App-link"
